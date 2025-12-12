2b0f693104523b134b15eb7bb5603f8acccbe9df
janlauridsen <jan.lauridsen@hotmail.com>
2025-12-12 13:20:49 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from "react";
+import AIChatButton from "../components/AIChatButton";
+import AIChat from "../components/AIChat";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Component {...pageProps} />
+
+      {/* AI CHAT BUBBLE */}
+      <AIChatButton onClick={() => setOpen(true)} />
+
+      {/* AI CHAT WINDOW */}
+      <AIChat open={open} onClose={() => setOpen(false)} />
+    </>
+  );
 }
