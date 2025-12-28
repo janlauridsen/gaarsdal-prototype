fe0de2d751f5b28970a70579f4610e32d50ebee5
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-28 09:12:58 +0100

    Create runtimeConfig.ts

---
+++ lib/runtime/runtimeConfig.ts
@@ -0,0 +1,10 @@
+// lib/runtime/runtimeConfig.ts
+
+export const runtimeConfig = {
+  runtimeProfile: "minimal",
+  bootstrapSnapshot: "RMRC_LOGGING_AND_ADMIN_CORE",
+  activeRoles: [
+    "spejler",
+    "afgraenser",
+  ],
+};
