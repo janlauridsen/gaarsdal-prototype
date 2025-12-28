c5ed83039eb0925bbcd1f1d548b7cfc578e844d3
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-28 08:55:21 +0100

    Update writeLogEvent.ts

--- lib/logging/writeLogEvent.ts
+++ lib/logging/writeLogEvent.ts
@@ -3,7 +3,9 @@
 import { RMRCLogEvent } from "./logEvent";
 import { redis } from "../redis";
 
-export async function writeLogEvent(event: RMRCLogEvent): Promise<void> {
+export async function writeLogEvent(
+  event: RMRCLogEvent
+): Promise<void> {
   const key = `rmrc:session:${event.sessionId}:events`;
 
   await redis.rpush(key, JSON.stringify(event));
