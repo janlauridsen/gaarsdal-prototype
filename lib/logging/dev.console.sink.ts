6755de40b697de3420e819a514ec49279df6cd28
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-29 10:27:26 +0100

    Delete lib directory

--- lib/logging/dev.console.sink.ts
+++
@@ -1,8 +0,0 @@
-import { LogSink } from "./logging.types"
-import { RMRCLogEntry } from "./logging.contract"
-
-export class ConsoleLogSink implements LogSink {
-  async write(entry: RMRCLogEntry): Promise<void> {
-    console.log("[RMRC LOG]", JSON.stringify(entry, null, 2))
-  }
-}
