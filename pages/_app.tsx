ef46f9bcdf7ed25d6ce4c9d99d667653cf383752
Jan Lauridsen <jan.lauridsen@hotmail.com>
2026-04-13 07:32:28 +0200

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,8 +34,8 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover, interactive-widget=resizes-content"
         />
-        <link rel="icon" type="image/png" href="/gaarsdal-logo-branding-notext.png" />
-        <link rel="apple-touch-icon" href="/gaarsdal-logo-branding-notext.png" />
+        <link rel="icon" type="image/png" href="/android-chrome-512x512.png" />
+        <link rel="apple-touch-icon" href="/android-chrome-512x512.png" />
       </Head>
       <Component {...pageProps} />
     </>
