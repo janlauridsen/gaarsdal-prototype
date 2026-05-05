3a457049487516327350e30d91ef5e74e882fc31
Jan Lauridsen <jan.lauridsen@hotmail.com>
2026-05-05 08:40:50 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer({ contact = { phone: '+45 42 80 74 74', email: 'j
         aria-hidden="true"
         style={{
           position: 'absolute',
-          top: '50%',
+          top: '75%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
           height: '440px',
