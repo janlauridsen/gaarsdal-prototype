9639d569ca280e027a26185f59e679f32b0e9984
Jan Lauridsen <jan.lauridsen@hotmail.com>
2026-05-14 14:48:03 +0200

    Update om.tsx

--- pages/om.tsx
+++ pages/om.tsx
@@ -39,7 +39,7 @@ export default function Om() {
                 width={120}
                 height={120}
                 className="rounded-xl object-cover shadow-sm"
-                style={{ opacity: 0.44 }}
+                style={{ opacity: 0.66 }}
                 priority
               />
             </div>
