2ecd337e9a0ee870fc926bd0588787075a01c934
Jan Lauridsen <jan.lauridsen@hotmail.com>
2026-06-01 09:10:50 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/foto',
+        destination: '/om',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
